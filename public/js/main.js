138c4cfd85a9bdace037983ec4fa0ddc0fc316ed
christianvilla <christianj.villa@gmail.com>
2020-10-08 20:40:52 -0700

    adds title transition and color change in js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 console.log("welcome to the killers club ⚔️");
+
+const killerTitle = document.getElementById("killer");
+
+console.log(killerTitle);
+
+// killerTitle.style.color = "red";
+
+window.addEventListener("load", (event) => {
+  setTimeout(function () {
+    killerTitle.style.color = "#ad310b";
+    killerTitle.style.transition = "color 3s";
+  }, 1500);
+});
