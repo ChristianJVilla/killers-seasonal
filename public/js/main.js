5878b28229ec06bc03f8fa9cd2a41942e83d99f2
christianvilla <christianj.villa@gmail.com>
2020-10-08 21:12:22 -0700

    put fonts and colors into variables

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,9 @@ console.log(killerTitle);
 // killerTitle.style.color = "red";
 
 window.addEventListener("load", (event) => {
+  killerTitle.style.opacity = "0";
   setTimeout(function () {
+    killerTitle.style.opacity = "1";
     killerTitle.style.color = "#ad310b";
     killerTitle.style.transition = "color 3s";
   }, 1500);
