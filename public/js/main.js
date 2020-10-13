b05e8013cca777f5b1882daaa29807959759eb53
christianvilla <christianj.villa@gmail.com>
2020-10-13 09:33:30 -0700

    adds landing button and animation for button

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,21 @@
 console.log("welcome to the killers club ⚔️");
 
 const killerTitle = document.getElementById("killer");
-
-console.log(killerTitle);
-
-// killerTitle.style.color = "red";
+const landingButton = document.getElementById("landing-btn");
 
 window.addEventListener("load", (event) => {
   killerTitle.style.opacity = "0";
+  landingButton.style.opacity = "0";
   setTimeout(function () {
     killerTitle.style.opacity = "1";
     killerTitle.style.color = "#ad310b";
     killerTitle.style.transition = "color 3s";
+    landingButton;
   }, 1500);
+  setTimeout(function () {
+    killerTitle.style.color = "rgb(124, 3, 3)";
+    killerTitle.style.transition = "color 3s";
+    landingButton.style.animation = "buttonFadeIn 2s ease";
+    landingButton.style.opacity = "1";
+  }, 3000);
 });
